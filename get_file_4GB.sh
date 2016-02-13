6671582596bda8b040d538e49cb98d0f91636751
Alexander Sczyrba <asczyrba@pizza.fritz.box>
2016-02-13 09:19:55 +0100

    initial version

---
+++ get_file_4GB.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+bibis3 --region=eu-west-1 -d s3://bibicloud-demo/kraken-db/minikraken_20140330.tar /vol/scratch/
+
+
---
+++ get_file_75GB.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+bibis3 --region=eu-west-1 -d s3://bibicloud-demo/kraken-db/kraken_medium.tar /vol/scratch/
+
+
