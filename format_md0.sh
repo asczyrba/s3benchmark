5b157adf423414901a1686e41c0cfef47bf15066
Ubuntu <ubuntu@ip-10-10-0-48.eu-west-1.compute.internal>
2016-02-18 13:21:49 +0000

    disable journaling on ext4

--- format_md0.sh
+++ format_md0.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 sudo umount /vol/scratch
-sudo mkfs.ext3 /dev/md0
+sudo mkfs.ext4 /dev/md0
+sudo tune2fs -o journal_data_write_back /dev/md0
+sudo tune2fs -O ^has_journal /dev/md0
 sudo mount /dev/md0 /vol/scratch/
 sudo chmod a+rwx /vol/scratch/
 df -T
