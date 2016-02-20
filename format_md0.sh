8e8a8b3361924965eb9afbe2e80f395ef246bdee
Ubuntu <ubuntu@ip-10-10-0-48.eu-west-1.compute.internal>
2016-02-20 07:47:36 +0000

    changed back to ext3

--- format_md0.sh
+++ format_md0.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 sudo umount /vol/scratch
-sudo mkfs.ext4 /dev/md0
-sudo tune2fs -o journal_data_write_back /dev/md0
-sudo tune2fs -O ^has_journal /dev/md0
+sudo mkfs.ext3 /dev/md0
 sudo mount /dev/md0 /vol/scratch/
 sudo chmod a+rwx /vol/scratch/
 df -T
