aa6647518f65a3aa1a39271742c03ae6b0fd696c
asczyrba <alexander.sczyrba@uni-bielefeld.de>
2016-04-16 08:36:55 +0200

    Update get_file_75GB.sh

--- get_file_75GB.sh
+++ get_file_75GB.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-bibis3 --region=eu-west-1 -d s3://bibicloud-demo/kraken-db/kraken_medium.tar /vol/scratch/
+bibis3 --region=eu-west-1 -d s3://bibicloud-demo/kraken-db/kraken_standard.tar /vol/scratch/
 
 
